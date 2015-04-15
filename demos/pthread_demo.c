bd471f39600286f4c63584a3f57a6be6a23ea8c8
secondwtq <lovejay-lovemusic@outlook.com>
2015-04-15 11:46:30 +0800

    Update.

--- demos/pthread_demo.c
+++ demos/pthread_demo.c
@@ -1,12 +1,21 @@
-// pthread & circular queue
 
-#include <stddef.h>
-#include <stdio.h>
-#include <pthread.h>
-#include <assert.h>
-#include <string.h>
+#define _XOPEN_SOURCE 500
+
+#include <stddef.h> // size_t
+#include <stdio.h> // sprintf
+#include <assert.h> // assert
+#include <string.h> // strlen, memset
+#include <ctype.h> // isspace, isdigit
+#include <stdlib.h> // atoi
+
+#include <fcntl.h> // open
+#include <unistd.h> // close, pread, write
+
+#include <pthread.h> // pthread family
 
 #define BUFSIZE 12
+
+//------------------------------------------------- BUFFER -------------------------------------------------
 
 struct final_buffer {
 	size_t count;
@@ -51,6 +60,7 @@ int fb_get(struct final_buffer *src) {
 }
 
 void fb_destroy(struct final_buffer *src) {
+	assert(src != NULL);
 	(void) src; // to supress "unused parameter" warning.
 }
 
@@ -71,6 +81,117 @@ void fb_test() {
 		printf("len: %lu\n", fb_count(&buf));
 	}
 }
+
+//------------------------------------------------- I/O -------------------------------------------------
+
+struct FILET {
+	int descriptor;
+	size_t offset;
+	int eof;
+	int closed;
+};
+
+void output_line(int a, int b) {
+	char stra[BUFSIZE] = { 0 }, strb[BUFSIZE] = { 0 };
+	char strp[BUFSIZE] = { 0 }, strm[BUFSIZE] = { 0 };
+	sprintf(stra, "%d", a); sprintf(strb, "%d", b);
+	sprintf(strp, "%d", a+b); sprintf(strm, "%d", a*b);
+
+	write(STDOUT_FILENO, stra, strlen(stra));
+	write(STDOUT_FILENO, " + ", 3);
+	write(STDOUT_FILENO, strb, strlen(strb));
+	write(STDOUT_FILENO, " = ", 3);
+	write(STDOUT_FILENO, strp, strlen(strp));
+	write(STDOUT_FILENO, " * ", 3);
+	write(STDOUT_FILENO, strb, strlen(strb));
+	write(STDOUT_FILENO, " = ", 3);
+	write(STDOUT_FILENO, strm, strlen(strm));
+	write(STDOUT_FILENO, "\n", 1);
+
+	return;
+}
+
+typedef int READSTATE;
+#define STATE_SPACE 0
+#define STATE_SHOULDHAVE 1
+#define STATE_NUM 2
+
+void open_file_foread(struct FILET *file, const char *path) {
+	assert(file != NULL);
+	assert(path != NULL);
+
+	int descriptor = open(path, O_RDONLY);
+	assert(descriptor != -1); // no exception safety
+
+	file->descriptor = descriptor;
+	file->offset = 0;
+	file->eof = 0;
+	file->closed = 0;
+
+	return;
+}
+
+void close_file(struct FILET *file) {
+	assert(file != NULL);
+	assert(file->closed == 0);
+
+	int t = close(file->descriptor);
+	assert(t == 0);
+
+	return;
+}
+
+int read_int(struct FILET *file) {
+	assert(file != NULL);
+
+	int ret = 0;
+
+	char buf[BUFSIZE+1] = { 0 };
+	char t = 0;
+
+	off_t cur_offset = file->offset;
+	READSTATE state = STATE_SPACE;
+
+	assert(file->eof == 0);
+
+	while (1) {
+		ssize_t not_eof = pread(file->descriptor, (void *)&t, 1, cur_offset);
+		cur_offset += 1;
+		if (!not_eof) {
+			file->offset = cur_offset;
+			file->eof = 1;
+			return 0;
+		}
+
+		switch (state) {
+			case STATE_SPACE:
+				if (isspace(t))
+					break;
+				else if (t == '-') {
+					buf[0] = t; state = STATE_SHOULDHAVE; }
+				else if (isdigit(t)) {
+					buf[0] = t; state = STATE_NUM; }
+				break;
+			case STATE_SHOULDHAVE:
+				if (isdigit(t)) {
+					buf[1] = t; state = STATE_NUM; }
+				break;
+			case STATE_NUM:
+				if (isdigit(t)) {
+					buf[strlen(buf)] = t;
+				} else if (isspace(t)) {
+					file->offset = cur_offset;
+					state = STATE_SPACE;
+					return atoi(buf);
+				}
+				break;
+		}
+	}
+
+	return ret;
+}
+
+//------------------------------------------------- NOW BACK TO THE TOPIC -------------------------------------------------
 
 typedef int WORKER_TYPE;
 
@@ -124,13 +245,13 @@ void *thread_worker(void *arg_raw) {
 		assert(args->path != NULL);
 		assert(args->buf != NULL);
 
-		FILE *fp = fopen(args->path, "r");
-		assert(fp != NULL);
+		struct FILET ft;
+		open_file_foread(&ft, args->path);
 
 		int t;
 		while (1) {
-			if (fscanf(fp, "%d", &t) == EOF)
-				break;
+			t = read_int(&ft);
+			if (ft.eof) break;
 			pthread_mutex_lock(&mutexes[args->id]); {
 				fb_put(args->buf, t);
 				// printf("%d putting %d ...\n", args->id, t);
@@ -138,6 +259,7 @@ void *thread_worker(void *arg_raw) {
 			pthread_mutex_unlock(&mutexes[args->id]);
 		}
 
+		close_file(&ft);
 		thread_running[args->id] = 0;
 
 	} else {
@@ -149,7 +271,8 @@ void *thread_worker(void *arg_raw) {
 				pthread_mutex_lock(&mutexes[i]); {
 				while (fb_count(&buf1) && fb_count(&buf2)) {
 					int a = fb_get(&buf1), b = fb_get(&buf2);
-					printf("%d + %d = %d, %d * %d = %d.\n", a, b, a + b, a, b, a * b);
+					output_line(a, b);
+					// printf("%d + %d = %d, %d * %d = %d.\n", a, b, a + b, a, b, a * b);
 				}
 				if (!(thread_running[0] || thread_running[1]))
 					time_to_leave = 1;
